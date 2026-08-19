facc64680d48f944ea9e9b9147be7d54ae464a56
bhagyahelawar7-jpg <bhagyahelawar7@gmail.com>
2026-08-19 22:00:00 +0530

    java challenges

---
+++ challenge2.java
@@ -0,0 +1,23 @@
+
+class challenge2{
+    static String college = "Garden City University";
+    static String name = "Bhagya ";
+    int age = 20;
+    public void information(){
+        int marks = 87;
+       
+        System.out.println("College : "+ college);
+         System.out.println("Student Name : "+ name);
+        System.out.println("Age : "+ age);
+        System.out.println("Marks: "+ marks);
+       
+        
+    }
+    public static void main(String[]args){
+    challenge2 c = new challenge2();
+    c.information();
+}
+
+}
+
+
